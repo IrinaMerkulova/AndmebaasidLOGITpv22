289d390051378d533c85604da02b0e2ad8ba02c4
Irina Merkulova <irina.merkulova@gmail.com>
2024-04-08 14:10:57 +0300

    Insert into on valmis!

--- TabeliLoomine.sql
+++ TabeliLoomine.sql
@@ -16,3 +16,11 @@ sisseastumis_kp date
 );
 --primary key - ? primaarne võti mis annab unikaalsus
 --identity(1,1) -? määrab igaühele oma numbri
+
+--andmete lisamine
+INSERT INTO opilane(
+eesnimi, perenimi, isikukood, aadress, sisseastumis_kp)
+VALUES
+('Ivan', 'Edu', '6784', 'Tallinn', '2023-08-16'),
+('Edu', 'Sulle', '234', 'Tartu', '2023-08-16');
+SELECT * FROM opilane;
