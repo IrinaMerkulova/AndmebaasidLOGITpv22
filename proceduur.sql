da82f041f9b14208267e0b7ecd7d4d90917c2a34
IrinaMerkulova <irina.merkulova@gmail.com>
2024-04-24 12:12:56 +0300

    proceduur otsing 1 täht

--- proceduur.sql
+++ proceduur.sql
@@ -6,3 +6,9 @@ kestvus int,
 rezisoor varchar(50),
 v_aasta int);
 
+CREATE PROCEDURE `otsing1taht`(IN `taht` CHAR(1)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER 
+  
+  begin
+	select * from film
+	where filmnimetus like concat(taht,'%');
+end;
